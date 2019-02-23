d193e1e6f1c97b7e540f5d9c4cb3a7987d56d04b
houimli safa <safa.houimli5@gmail.com>
2019-02-23 16:05:54 -0500

    Creation d'une base de données MongoDB rssDB

--- main_app/GUI/add_rss_gui.py
+++ main_app/GUI/add_rss_gui.py
@@ -8,13 +8,14 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_AddRssWindow(object):
     def addRss(self):
         self.rssAddress = self.rss_adress_lineEdit.text()
-        self.rssCategory = self.category_lineEdit.text()
+        self.rssCategory = self.category_comboBox.currentText()
+        self.updateFrequency = self.update_frequency_comboBox.currentText()
         print(self.rssAddress)
         print(self.rssCategory)
+        print(self.updateFrequency)
 
     def setupUi(self, AddRssWindow):
         AddRssWindow.setObjectName("AddRssWindow")
@@ -41,9 +42,10 @@ class Ui_AddRssWindow(object):
 "background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 170, 0, 255), stop:1 rgba(255, 255, 255, 255));")
         self.label_3.setObjectName("label_3")
         self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
-        self.category_lineEdit = QtWidgets.QLineEdit(self.frame)
-        self.category_lineEdit.setObjectName("category_lineEdit")
-        self.gridLayout.addWidget(self.category_lineEdit, 1, 1, 1, 1)
+        self.category_comboBox = QtWidgets.QComboBox(self.frame)
+        self.category_comboBox.setObjectName("category_lineEdit")
+        self.gridLayout.addWidget(self.category_comboBox, 1, 1, 1, 1)
+        self.category_comboBox.addItems(["Actualité","Sport","Politique","Technologie","Célébrités","Fashion","Autres"])
         self.label_2 = QtWidgets.QLabel(self.frame)
         self.label_2.setStyleSheet("font: 10pt \"MS Shell Dlg 2\";\n"
 "background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 170, 0, 255), stop:1 rgba(255, 255, 255, 255));")
@@ -52,6 +54,8 @@ class Ui_AddRssWindow(object):
         self.update_frequency_comboBox = QtWidgets.QComboBox(self.frame)
         self.update_frequency_comboBox.setObjectName("update_frequency_comboBox")
         self.gridLayout.addWidget(self.update_frequency_comboBox, 2, 1, 1, 1)
+        self.update_frequency_comboBox.clear()
+        self.update_frequency_comboBox.addItems(["15 minutes", "30 minutes", "45 minutes"])
         self.frame_2 = QtWidgets.QFrame(self.centralwidget)
         self.frame_2.setGeometry(QtCore.QRect(70, 150, 311, 51))
         self.frame_2.setStyleSheet("font: 9pt \"MS Shell Dlg 2\";")
@@ -63,12 +67,12 @@ class Ui_AddRssWindow(object):
         self.btn_submit_add = QtWidgets.QPushButton(self.frame_2)
         self.btn_submit_add.setObjectName("btn_submit_add")
         self.gridLayout_2.addWidget(self.btn_submit_add, 0, 0, 1, 1)
-        # self.btn_submit_add.triggered.connect(self.addRss)
+        self.btn_submit_add.clicked.connect(self.addRss)
 
         self.btn_cancel_add = QtWidgets.QPushButton(self.frame_2)
         self.btn_cancel_add.setObjectName("btn_cancel_add")
         self.gridLayout_2.addWidget(self.btn_cancel_add, 0, 1, 1, 1)
-        # self.btn_cancel_add.triggered.connect(AddRssWindow.close)
+        self.btn_cancel_add.clicked.connect(AddRssWindow.close)
 
         AddRssWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(AddRssWindow)
@@ -91,6 +95,7 @@ class Ui_AddRssWindow(object):
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyle('Fusion')
     AddRssWindow = QtWidgets.QMainWindow()
     ui = Ui_AddRssWindow()
     ui.setupUi(AddRssWindow)
--- main_app/main.py
+++ main_app/main.py
@@ -2,7 +2,6 @@ import sys
 from PyQt5 import QtWidgets
 from main_app.GUI.rss_gui import Ui_MainWindow
 
-
 class MainWindow(Ui_MainWindow):
     def __init__(self, dialog):
         Ui_MainWindow.__init__(self)
@@ -11,37 +10,9 @@ class MainWindow(Ui_MainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyle('Fusion')
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
-# if __name__ == '__main__':
-#     app = QtWidgets.QApplication(sys.argv)
-#     # Style
-#     app.setStyle('Fusion')
-#     dark_palette = QPalette()
-#
-#     dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
-#     dark_palette.setColor(QPalette.WindowText, Qt.white)
-#     dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
-#     dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-#     dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
-#     dark_palette.setColor(QPalette.ToolTipText, Qt.white)
-#     dark_palette.setColor(QPalette.Text, Qt.white)
-#     dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
-#     dark_palette.setColor(QPalette.ButtonText, Qt.white)
-#     dark_palette.setColor(QPalette.BrightText, Qt.red)
-#     dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
-#     dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-#     dark_palette.setColor(QPalette.HighlightedText, Qt.black)
-#
-#     app.setPalette(dark_palette)
-#     app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
-#
-#     dialog = QtWidgets.QDialog()
-#     prog = MainWindow(dialog)
-#     dialog.show()
-#     sys.exit(app.exec_())
-
